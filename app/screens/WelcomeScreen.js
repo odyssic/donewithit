7e9aacfd0de5243275daae242dfb238b348f87c1
odyssic <stevenbuddenjr@gmail.com>
2020-09-23 17:30:30 -0400

    working basic item view

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native'
 
 export default function WelcomeScreen() {
     return (
         <ImageBackground
             style={styles.background}
             source={require("../assets/background.jpg")}>
+            <View style={styles.logoContainer}>
+                <Image
+                    style={styles.logo}
+                    source={require("../assets/logo-red.png")}></Image>
+
+                <Text>Sell what you don't need</Text>
+            </View>
+
+            <View style={styles.loginButton}>
+
+            </View>
+            <View style={styles.registerButton}>
+
+            </View>
         </ImageBackground>
     )
 }
@@ -13,7 +27,36 @@ export default function WelcomeScreen() {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-    }
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    logo: {
+
+        width: 100,
+        height: 100,
+
+
+    },
+    logoContainer: {
+        position: "absolute",
+        top: 70,
+        alignItems: "center",
+
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#fc5c65"
+
+
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#4ecdc4"
+
+    },
+
 
 });
 
