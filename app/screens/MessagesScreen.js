e49e059129ac9be0ec338abd7800717d939c80a2
odyssic <stevenbuddenjr@gmail.com>
2020-09-30 10:16:23 -0400

    delete function swipe button working, no working on delete functionality

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,46 +1,68 @@
-import React from 'react'
-import { FlatList, SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from 'react-native'
-import ListItem from '../components/ListItem'
-import Constants from 'expo-constants';
+import React, { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 
-console.log(Constants)
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 
-const messages = [
+const initialMessages = [
     {
         id: 1,
-        title: 'T1',
-        description: 'D1',
-        image: require('../assets/mosh.jpg')
+        title: "T1",
+        description: "D1",
+        image: require("../assets/mosh.jpg"),
     },
     {
         id: 2,
-        title: 'T2',
-        description: 'D2',
-        image: require('../assets/mosh.jpg')
-    }
+        title: "T2",
+        description: "D2",
+        image: require("../assets/mosh.jpg"),
+    },
+];
 
-]
+function MessagesScreen(props) {
+    const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
 
-export default function MessagesScreen() {
+    const handleDelete = (message) => {
+        // Delete the message from messages
+        setMessages(messages.filter((m) => m.id !== message.id));
+    };
+
     return (
-        <SafeAreaView style={styles.screen}>
-            <View>
-                <FlatList
-                    data={messages}
-                    keyExtractor={message => message.id.toString()}
-                    renderItem={({ item }) => <ListItem
+        <Screen>
+            <FlatList
+                data={messages}
+                keyExtractor={(message) => message.id.toString()}
+                renderItem={({ item }) => (
+                    <ListItem
                         title={item.title}
                         subTitle={item.description}
                         image={item.image}
-                    ></ListItem>} />
-
-            </View >
-        </SafeAreaView>
-    )
+                        onPress={() => console.log("Message selected", item)}
+                        renderRightActions={() => (
+                            <ListItemDeleteAction onPress={() => handleDelete(item)} />
+                        )}
+                    />
+                )}
+                ItemSeparatorComponent={ListItemSeparator}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setMessages([
+                        {
+                            id: 2,
+                            title: "T2",
+                            description: "D2",
+                            image: require("../assets/mosh.jpg"),
+                        },
+                    ]);
+                }}
+            />
+        </Screen>
+    );
 }
 
-const styles = StyleSheet.create({
-    screen: {
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-    }
-})
+const styles = StyleSheet.create({});
+
+export default MessagesScreen;
