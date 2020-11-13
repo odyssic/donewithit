ad8051cdc10979f38d80e364f647d55f4a18fa68
odyssic <stevenbuddenjr@gmail.com>
2020-11-13 12:00:05 -0500

    adding files for progress bar

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
@@ -11,6 +11,7 @@ import listingsApi from '../api/listings';
 import Screen from "../components/Screen";
 import SubmitButton from "../components/forms/SubmitButton"; 
 import useLocation from "../hooks/useLocation";
+import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -79,17 +80,27 @@ const categories = [
 
 function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const handleSubmit = async listing => {
-    const result = await listingsApi.addListing({ ...listing, location })
-    if (!result.ok)
+    setProgress(0);
+    setUploadVisible(true)
+    const result = await listingsApi.addListing({ ...listing, location },
+      progress => setProgress(progress)
+    )
+
+    if (!result.ok) {
+      setUploadVisible(false);
       return alert('Could not save the listing.');
-    alert('Success');
+    }
+    
     
   }
 
   return (
     <Screen style={styles.container}>
+      <UploadScreen onDone={ ()=> setUploadVisible(false)}progress={progress} visble={uploadVisible}/>
       <AppForm
         initialValues={{
           title: "",
